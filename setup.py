f4e3d533592ca1ea955e88441943c880333d4ffa
Yoshiteru Hidaka <yhidaka@bnl.gov>
2019-09-25 13:10:03 -0400

    made compatible with sdist option

--- setup.py
+++ setup.py
@@ -7,21 +7,29 @@ import os
 import json
 
 #print(sys.argv)
-facility_name = sys.argv[2]
-available_facility_names = ['nsls2apcluster',]
-if facility_name not in available_facility_names:
-    print('* Only the following facility names are available:')
-    print('      ' + ', '.join(available_facility_names))
-    raise ValueError(
-        'Specified facility_name "{}" is not available.'.format(facility_name))
 
-this_folder = os.path.dirname(os.path.abspath(__file__))
 facility_json_filename = 'facility.json'
-with open(os.path.join(
-    this_folder, 'src', 'pyelegant', facility_json_filename), 'w') as f:
-    json.dump({'name': facility_name}, f)
 
-sys.argv = sys.argv[:2]
+if ('install' in sys.argv) or ('sdist' in sys.argv):
+
+    facility_name = sys.argv[2]
+    available_facility_names = ['nsls2apcluster',]
+    if facility_name not in available_facility_names:
+        print('* Only the following facility names are available:')
+        print('      ' + ', '.join(available_facility_names))
+        raise ValueError(
+            'Specified facility_name "{}" is not available.'.format(facility_name))
+
+    this_folder = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(
+        this_folder, 'src', 'pyelegant', facility_json_filename), 'w') as f:
+        json.dump({'name': facility_name}, f)
+
+    sys.argv = sys.argv[:2]
+
+    if 'sdist' in sys.argv:
+        # Modify the tarball name to be "pyelegant-{facility_name}-?.?.?.tar.gz"
+        program_name += f'-{facility_name}'
 
 setup(
     name = program_name,
