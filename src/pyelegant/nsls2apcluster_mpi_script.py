56ea66702e58e3a48ffac09ffa6005dbd0fd6e25
Yoshiteru Hidaka <yhidaka@bnl.gov>
2020-03-18 23:26:54 -0400

    removing unnecessary and potentially inconsistent ELEGANT module env setup for nsls2apcluster_mpi_script.py

--- src/pyelegant/nsls2apcluster_mpi_script.py
+++ src/pyelegant/nsls2apcluster_mpi_script.py
@@ -7,56 +7,12 @@ import tempfile
 from pathlib import Path
 from functools import partial
 import traceback
-from subprocess import Popen, PIPE
-import shlex
 
 from mpi4py import MPI
 from mpi4py.futures import MPIPoolExecutor
 import dill
 # To allow class methods usable in mpi4py
 MPI.pickle.__init__(dill.dumps, dill.loads) # <= This works for mpi4py v.3.0
-
-_ABS_TIME_LIMIT = None
-#_ABS_TIME_LIMIT = '2019-10-24T05-50-00'
-SLURM_ABS_TIME_LIMIT = {}
-for _k in ['normal', 'tiny', 'short', 'low', 'high', 'long', 'longlong', 'debug',
-           'risky']:
-    SLURM_ABS_TIME_LIMIT[_k] = _ABS_TIME_LIMIT
-
-SLURM_EXCL_NODES = None
-#SLURM_EXCL_NODES = ['apcpu-005',]
-#SLURM_EXCL_NODES = ['cpu-019', 'cpu-020', 'cpu-021',]
-#SLURM_EXCL_NODES = [f'cpu-{n:03d}' for n in range(19, 26+1)] # exclude GPFS nodes
-#SLURM_EXCL_NODES = [f'cpu-{n:03d}' for n in
-                    #list(range(2, 5+1)) + list(range(7, 15+1))] # exclude NFS nodes
-#SLURM_EXCL_NODES = [
-    #f'cpu-{n:03d}' for n in list(range(19, 26+1)) + list(range(2, 5+1)) +
-    #list(range(7, 15+1))] # exclude both GPFS & NFS nodes
-#SLURM_EXCL_NODES = [f'apcpu-{n:03d}' for n in range(1, 5+1)] + [
-    #f'cpu-{n:03d}' for n in list(range(2, 5+1)) +
-    #list(range(7, 15+1))] # exclude both apcpu & NFS nodes, i.e., including only GPFS nodes
-
-MODULE_LOAD_CMD_STR = 'elegant-latest'
-#MODULE_LOAD_CMD_STR = 'elegant-latest elegant/2019.2.1'
-
-_p = Popen(shlex.split('which elegant'), stdout=PIPE, stderr=PIPE, encoding='utf-8')
-_out, _err = _p.communicate()
-if _out.strip() == '':
-    nMaxTry = 3
-    for iTry in range(nMaxTry):
-        try:
-            # Load "Elegant" module (for Lmod v.8.1)
-            sys.path.insert(0, os.path.join(os.environ['MODULESHOME'], 'init'))
-            from env_modules_python import module
-            #module('load', 'gcc', 'mpich', 'elegant')
-            module('load', *MODULE_LOAD_CMD_STR.split())
-            print('Elegant module loaded successfully')
-            break
-        except:
-            if iTry != nMaxTry - 1:
-                time.sleep(5.0)
-            else:
-                raise RuntimeError('# Loading module "elegant" failed.')
 
 #----------------------------------------------------------------------
 def _mpi_starmap(func_or_classmethod, param_list, *args):
