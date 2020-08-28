f7926abaee684891cc0fc9538eafa7774ead668b
Yoshiteru Hidaka <yhidaka@bnl.gov>
2020-08-27 20:53:09 -0400

    addition of analytical ORM calc func & major upgrade for ClosedOrbitCaculator (persistent element property changes, checkpointing, more set/get property functions)

--- src/pyelegant/orbit.py
+++ src/pyelegant/orbit.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import numpy as np
 import matplotlib.pylab as plt
 import tempfile
+from copy import deepcopy
 
 from .local import run
 from .remote import remote
@@ -11,9 +12,10 @@ from . import __version__, std_print_enabled
 from . import elebuilder
 from . import util
 from . import sdds
+from . import twiss
 
 def get_closed_orbit(
-    ele_filepath: str, clo_output_filepath: str,
+    ele_filepath: str, clo_output_filepath: str, param_output_filepath: str = '',
     run_local: bool = True, remote_opts: Optional[dict] = None,
     ) -> Tuple[dict]:
     """"""
@@ -36,6 +38,8 @@ def get_closed_orbit(
                    output_filepaths=None)
 
     tmp_filepaths = dict(clo=clo_output_filepath)
+    if Path(param_output_filepath).exists():
+        tmp_filepaths['param'] = param_output_filepath
     output, meta = {}, {}
     for k, v in tmp_filepaths.items():
         try:
@@ -282,11 +286,11 @@ class ClosedOrbitCalculator:
         assert n_turns >= 0
         assert iteration_fraction <= 1.0
 
-        self.columns = None
-        self.params = None
-
         self.hcors = {}
         self.vcors = {}
+
+        self._mod_props = {}
+        self._checkpoints = {}
 
         self.make_tempdir(tempdir_path=tempdir_path)
 
@@ -296,9 +300,14 @@ class ClosedOrbitCalculator:
                 suffix='.ele')
             self.ele_filepath = os.path.abspath(tmp.name)
             tmp.close()
-
         else:
             self.ele_filepath = ele_filepath
+
+        if self.ele_filepath.endswith('.ele'):
+            self.param_filepath = self.ele_filepath[:-4] + '.param'
+        else:
+            self.param_filepath = self.ele_filepath + '.param'
+        self._param_output = {}
 
         self.ed = ed = elebuilder.EleDesigner(self.ele_filepath, double_format='.12g')
 
@@ -308,7 +317,8 @@ class ClosedOrbitCalculator:
             ed.add_newline()
 
         ed.add_block('run_setup',
-            lattice=LTE_filepath, p_central_mev=E_MeV, use_beamline=use_beamline)
+            lattice=LTE_filepath, p_central_mev=E_MeV, use_beamline=use_beamline,
+            parameters=self.param_filepath)
 
         ed.add_newline()
 
@@ -359,6 +369,8 @@ class ClosedOrbitCalculator:
         for fp in ed.actual_output_filepath_list:
             if fp.endswith('.clo'):
                 self.clo_output_filepath = fp
+            elif fp.endswith('.param'):
+                pass
             else:
                 raise ValueError('This line should not be reached.')
 
@@ -377,6 +389,11 @@ class ClosedOrbitCalculator:
         """"""
 
         self.tempdir.cleanup()
+
+    def getLattice(self):
+        """"""
+
+        return self.ed.getLattice()
 
     def get_all_available_kickers(self, spos_sorted=True):
         """"""
@@ -451,32 +468,102 @@ class ClosedOrbitCalculator:
         assert len(vkick_rads) == len(self.vcors['rads'])
         self.vcors['rads'] = vkick_rads
 
-        col = dict(
-            ElementName=(self.hcors['names'].tolist() +
-                         self.vcors['names'].tolist()),
-            ElementParameter=(
-                self.hcors['kick_prop_names'] +
-                self.vcors['kick_prop_names']),
-            ParameterValue=np.append(self.hcors['rads'], self.vcors['rads']))
+        elem_names = np.append(self.hcors['names'], self.vcors['names'])
+        elem_prop_names = self.hcors['kick_prop_names'] + self.vcors['kick_prop_names']
+        elem_prop_vals = np.append(self.hcors['rads'], self.vcors['rads'])
 
-        sdds.dicts2sdds(
-            self.corrector_params_filepath, params=None, columns=col,
-            outputMode='binary', suppress_err_msg=True)
+        assert len(elem_names) == len(elem_prop_names) == len(elem_prop_vals)
+
+        for elem_name, param_name, param_val in zip(
+            elem_names, elem_prop_names, elem_prop_vals):
+
+            self._mod_props[(elem_name, param_name)] = param_val
+
+        self._write_to_parameters_file()
 
     def get_kick_angles(self) -> Dict:
         """"""
 
         return dict(h=self.hcors['rads'], v=self.vcors['rads'])
 
-
     def set_elem_properties(self, elem_names, elem_prop_names, elem_prop_vals):
         """"""
 
         assert len(elem_names) == len(elem_prop_names) == len(elem_prop_vals)
 
-        col = dict(
-            ElementName=elem_names, ElementParameter=elem_prop_names,
-            ParameterValue=elem_prop_vals)
+        for elem_name, param_name, param_val in zip(
+            elem_names, elem_prop_names, elem_prop_vals):
+
+            k = (elem_name, param_name)
+            self._mod_props[k] = param_val
+
+        self._write_to_parameters_file()
+
+    def get_modified_elem_properties(self, elem_names, elem_prop_names,
+                                     ret_dict=False):
+        """"""
+
+        assert len(elem_names) == len(elem_prop_names)
+
+        if ret_dict:
+            out = {}
+        else:
+            out = np.full((len(elem_names),), np.nan)
+
+        for i, (elem_name, param_name) in enumerate(zip(
+            elem_names, elem_prop_names)):
+
+            k = (elem_name, param_name)
+
+            if k in self._mod_props:
+                val = self._mod_props[k]
+            else:
+                val = np.nan
+
+            if ret_dict:
+                out[k] = val
+            else:
+                out[i] = val
+
+        return out
+
+    def get_elem_properties(self, elem_names, elem_prop_names, ret_dict=False):
+        """"""
+
+        assert len(elem_names) == len(elem_prop_names)
+
+        if ret_dict:
+            out = {}
+        else:
+            out = np.full((len(elem_names),), np.nan)
+
+        for i, (elem_name, param_name) in enumerate(zip(
+            elem_names, elem_prop_names)):
+
+            k = (elem_name, param_name)
+
+            try:
+                val = self._param_output['ParameterValue'][
+                    (self._param_output['ElementName'] == elem_name) &
+                    (self._param_output['ElementParameter'] == param_name)][0]
+            except:
+                val = np.nan
+
+            if ret_dict:
+                out[k] = val
+            else:
+                out[i] = val
+
+        return out
+
+    def _write_to_parameters_file(self):
+        """"""
+
+        col = dict(ElementName=[], ElementParameter=[], ParameterValue=[])
+        for (elem_name, param_name), param_val in self._mod_props.items():
+            col['ElementName'].append(elem_name)
+            col['ElementParameter'].append(param_name)
+            col['ParameterValue'].append(param_val)
 
         sdds.dicts2sdds(
             self.corrector_params_filepath, params=None, columns=col,
@@ -506,6 +593,7 @@ class ClosedOrbitCalculator:
 
         data, meta= get_closed_orbit(
             self.ele_filepath, self.clo_output_filepath,
+            param_output_filepath=self.param_filepath,
             run_local=run_local, remote_opts=remote_opts)
 
         if data == {}:
@@ -517,6 +605,8 @@ class ClosedOrbitCalculator:
             self.clo_columns = data['clo']['columns']
             self.clo_params = data['clo']['params']
 
+            self._param_output = data['param']['columns']
+
         return dict(columns=self.clo_columns, params=self.clo_params)
 
     def plot(self):
@@ -524,3 +614,92 @@ class ClosedOrbitCalculator:
 
         plot_closed_orbit(self.clo_columns, self.clo_params)
 
+    def save_checkpoint(self, name='last'):
+        """"""
+
+        self._checkpoints[name] = deepcopy(self._mod_props)
+
+    def load_checkpoint(self, name='last'):
+        """"""
+
+        self._mod_props.clear()
+        self._mod_props.update(self._checkpoints[name])
+
+        self._write_to_parameters_file()
+
+def calcAnalyticalORM(
+    LTE_filepath, E_MeV, bpm_elem_inds_or_name_occur_tups,
+    hcor_elem_inds_or_name_occur_tups, vcor_elem_inds_or_name_occur_tups,
+    use_beamline=None):
+    """
+    No transverse coupling assumed
+    """
+
+    with tempfile.NamedTemporaryFile(suffix='.pgz') as f:
+        output_filepath = f.name
+        twiss.calc_ring_twiss(
+            output_filepath, LTE_filepath, E_MeV, use_beamline=use_beamline)
+
+        twi = util.load_pgz_file(output_filepath)
+
+    twi_arrays = twi['data']['twi']['arrays']
+    twi_scalars = twi['data']['twi']['scalars']
+
+    betax = twi_arrays['betax']
+    betay = twi_arrays['betay']
+    phix = twi_arrays['psix'] # [rad]
+    phiy = twi_arrays['psiy'] # [rad]
+    etax = twi_arrays['etax']
+    etay = twi_arrays['etay']
+    nux, nuy = twi_scalars['nux'], twi_scalars['nuy']
+    alphac = twi_scalars['alphac']
+
+    circumf = twi_arrays['s'][-1]
+
+    if isinstance(bpm_elem_inds_or_name_occur_tups[0], (int, np.integer)):
+        bpm_inds = bpm_elem_inds_or_name_occur_tups
+    else:
+        bpm_inds = np.array([
+            np.where((twi_arrays['ElementName'] == name) &
+                     (twi_arrays['ElementOccurence'] == occur))[0][0]
+            for name, occur in bpm_elem_inds_or_name_occur_tups])
+    nBPM = len(bpm_inds)
+
+    if isinstance(hcor_elem_inds_or_name_occur_tups[0], (int, np.integer)):
+        hcor_inds = np.array(hcor_elem_inds_or_name_occur_tups)
+    else:
+        hcor_inds = np.array([
+            np.where((twi_arrays['ElementName'] == name) &
+                     (twi_arrays['ElementOccurence'] == occur))[0][0]
+            for name, occur in hcor_elem_inds_or_name_occur_tups])
+    nHCOR = len(hcor_inds)
+
+    if isinstance(vcor_elem_inds_or_name_occur_tups[0], (int, np.integer)):
+        vcor_inds = np.array(vcor_elem_inds_or_name_occur_tups)
+    else:
+        vcor_inds = np.array([
+            np.where((twi_arrays['ElementName'] == name) &
+                     (twi_arrays['ElementOccurence'] == occur))[0][0]
+            for name, occur in vcor_elem_inds_or_name_occur_tups])
+    nVCOR = len(vcor_inds)
+
+    M = np.zeros((2*nBPM, nHCOR+nVCOR))
+
+    for i in range(nBPM):
+        cor_betax_avg = (betax[hcor_inds-1] + betax[hcor_inds]) / 2
+        cor_phix_avg = (phix[hcor_inds-1] + phix[hcor_inds]) / 2
+        cor_etax_avg = (etax[hcor_inds-1] + etax[hcor_inds]) / 2
+        M[i,:nHCOR] = np.sqrt(cor_betax_avg * betax[bpm_inds[i]]) * np.cos(
+            np.pi * nux - np.abs(cor_phix_avg - phix[bpm_inds[i]])) / (
+                2 * np.sin(np.pi * nux)
+            ) + cor_etax_avg * etax[bpm_inds[i]] / alphac / circumf
+
+        cor_betay_avg = (betay[vcor_inds-1] + betay[vcor_inds]) / 2
+        cor_phiy_avg = (phiy[vcor_inds-1] + phiy[vcor_inds]) / 2
+        cor_etay_avg = (etay[vcor_inds-1] + etay[vcor_inds]) / 2
+        M[i+nBPM,nHCOR:] = np.sqrt(cor_betay_avg * betay[bpm_inds[i]]) * np.cos(
+            np.pi * nuy - np.abs(cor_phiy_avg - phiy[bpm_inds[i]])) / (
+                2 * np.sin(np.pi * nuy)
+            ) + cor_etay_avg * etay[bpm_inds[i]] / alphac / circumf
+
+    return M
