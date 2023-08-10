7d1762f32bf87520060a1f0ff9129bcf9abee8b9
Yoshiteru Hidaka <yhidaka@bnl.gov>
2023-08-10 14:20:45 -0400

    pre-commit applied to remote.py

--- src/pyelegant/remote.py
+++ src/pyelegant/remote.py
@@ -4,13 +4,13 @@ import importlib
 
 this_folder = os.path.dirname(os.path.abspath(__file__))
 
-with open(os.path.join(this_folder, 'facility.json'), 'r') as f:
-    facility_name = json.load(f)['name']
+with open(os.path.join(this_folder, "facility.json"), "r") as f:
+    facility_name = json.load(f)["name"]
 
 try:
-    remote = importlib.import_module('.'+facility_name, 'pyelegant')
+    remote = importlib.import_module("." + facility_name, "pyelegant")
 except:
-    print('\n## pyelegant:WARNING ##')
+    print("\n## pyelegant:WARNING ##")
     print('Failed to load remote run setup for "{}"'.format(facility_name))
-    print('All the Elegant commands will only be run locally.')
+    print("All the Elegant commands will only be run locally.")
     remote = None
